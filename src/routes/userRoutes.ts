5720f8d52d5e4c9c70871be540bfb82621d62caa
Michal Mankiewicz <michal.mankiewicz@itechart-group.com>
2023-10-11 21:11:33 +0200

    feat: Added prisma and extended user routes logic

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,29 +1,72 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
+const prisma = new PrismaClient();
 
 // Create User
-router.post("/", (req, res) => {
-  res.status(501).json({ error: "Not impplemented" });
+router.post("/", async (req, res) => {
+  const { email, name, username } = req.body;
+
+  try {
+    const result = await prisma.user.create({
+      data: {
+        email,
+        name,
+        username,
+        bio: "XXX",
+      },
+    });
+
+    res.json(result);
+  } catch (e) {
+    res.status(400).json({ error: "Username and email should be unique" });
+  }
 });
 
 // List Users
-router.get("/", (req, res) => {
-  res.status(501).json({ error: "Not impplemented" });
+router.get("/", async (req, res) => {
+  const allUser = await prisma.user.findMany();
+  res.json(allUser);
 });
 
 // Get one User
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
+  const user = await prisma.user.findUnique({ where: { id: Number(id) } });
+
+  res.json(user);
+});
+
+// Update User
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { bio, name, image } = req.body;
+
+  try {
+    const result = await prisma.user.update({
+      where: { id: Number(id) },
+      data: {
+        bio,
+        image,
+        name,
+      },
+    });
+
+    res.json(result);
+  } catch (e) {
+    res.status(400).json({ error: "Username and email should be unique" });
+  }
 
   res.status(501).json({ error: `Not impplemented ${id}` });
 });
 
-// Update User
-router.put("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   const { id } = req.params;
 
-  res.status(501).json({ error: `Not impplemented ${id}` });
+  await prisma.user.delete({ where: { id: Number(id) } });
+
+  res.sendStatus(200);
 });
 
 export default router;
