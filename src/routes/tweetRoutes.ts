7ccb16c92a1ec8da06cbc1922eca872b43a4f11a
Michal Mankiewicz <michal.mankiewicz@itechart-group.com>
2023-10-14 21:20:50 +0200

    feat:Include relationship

--- src/routes/tweetRoutes.ts
+++ src/routes/tweetRoutes.ts
@@ -1,36 +1,71 @@
 import { Router } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const router = Router();
+const prisma = new PrismaClient();
+
+// Tweet CRUD
 
 // Create Tweet
-router.post("/", (req, res) => {
-  res.status(501).json({ error: "Not impplemented" });
+router.post("/", async (req, res) => {
+  const { content, image, userId } = req.body;
+
+  try {
+    const result = await prisma.tweet.create({
+      data: {
+        content,
+        image,
+        userId, // TODO manage based on the auth user
+      },
+    });
+
+    res.json(result);
+  } catch (e) {
+    res.status(400).json({ error: "Username and email should be unique" });
+  }
 });
 
-// List Tweet
-router.get("/", (req, res) => {
-  res.status(501).json({ error: "Not impplemented" });
+// list Tweet
+router.get("/", async (req, res) => {
+  const allTweets = await prisma.tweet.findMany({
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          username: true,
+          image: true,
+        },
+      },
+    },
+  });
+  res.json(allTweets);
 });
 
-// Get one Tweet
-router.get("/:id", (req, res) => {
+// get one Tweet
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
-
-  res.status(501).json({ error: `Not impplemented ${id}` });
+  const tweet = await prisma.tweet.findUnique({
+    where: { id: Number(id) },
+    include: { user: true },
+  });
+  if (!tweet) {
+    return res.status(404).json({ error: "Tweet not found!" });
+  }
+  res.json(tweet);
 });
 
-// Update tweet
+// update Tweet
 router.put("/:id", (req, res) => {
   const { id } = req.params;
-
-  res.status(501).json({ error: `Not impplemented ${id}` });
+  res.status(501).json({ error: `Not Implemented: ${id}` });
 });
 
-// Delete tweet
-router.delete("/:id", (req, res) => {
+// delete Tweet
+router.delete("/:id", async (req, res) => {
   const { id } = req.params;
-
-  res.status(501).json({ error: `Not impplemented ${id}` });
+  await prisma.tweet.delete({ where: { id: Number(id) } });
+  res.sendStatus(200);
 });
 
 export default router;
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,7 +4,17 @@ import { PrismaClient } from "@prisma/client";
 const router = Router();
 const prisma = new PrismaClient();
 
-// Create User
+// User CRUD
+
+/*
+  Test with curl:
+
+  curl -X POST -H "Content-Type: application/json" \
+       -d '{"name": "Elon Musk", "email": "doge@twitter.com", "username": "elon"}' \
+       http://localhost:3000/user/
+
+*/
+// Create user
 router.post("/", async (req, res) => {
   const { email, name, username } = req.body;
 
@@ -14,7 +24,7 @@ router.post("/", async (req, res) => {
         email,
         name,
         username,
-        bio: "XXX",
+        bio: "Hello, I'm new on Twitter",
       },
     });
 
@@ -24,21 +34,40 @@ router.post("/", async (req, res) => {
   }
 });
 
-// List Users
+// list users
 router.get("/", async (req, res) => {
-  const allUser = await prisma.user.findMany();
+  const allUser = await prisma.user.findMany({
+    // select: {
+    //   id: true,
+    //   name: true,
+    //   image: true,
+    //   bio: true,
+    // },
+  });
+
   res.json(allUser);
 });
 
-// Get one User
+// get one user
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  const user = await prisma.user.findUnique({ where: { id: Number(id) } });
+  const user = await prisma.user.findUnique({
+    where: { id: Number(id) },
+    include: { tweets: true },
+  });
 
   res.json(user);
 });
 
-// Update User
+/*
+  Test with curl:
+
+  curl -X PUT -H "Content-Type: application/json" \
+       -d '{"name": "Vadim", "bio": "Hello there!"}' \
+       http://localhost:3000/user/1
+
+*/
+// update user
 router.put("/:id", async (req, res) => {
   const { id } = req.params;
   const { bio, name, image } = req.body;
@@ -46,26 +75,19 @@ router.put("/:id", async (req, res) => {
   try {
     const result = await prisma.user.update({
       where: { id: Number(id) },
-      data: {
-        bio,
-        image,
-        name,
-      },
+      data: { bio, name, image },
     });
-
     res.json(result);
   } catch (e) {
-    res.status(400).json({ error: "Username and email should be unique" });
+    res.status(400).json({ error: `Failed to update the user` });
   }
-
-  res.status(501).json({ error: `Not impplemented ${id}` });
 });
 
+// curl -X DELETE http://localhost:3000/user/6
+// delete user
 router.delete("/:id", async (req, res) => {
   const { id } = req.params;
-
   await prisma.user.delete({ where: { id: Number(id) } });
-
   res.sendStatus(200);
 });
 
